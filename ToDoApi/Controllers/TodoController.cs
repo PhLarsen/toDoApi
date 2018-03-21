ff14b5252642f2fd320e745cd2a7416e182dc723
Philip Larsen <pcln3@hotmail.com>
2018-03-21 20:39:58 +0100

    Added CRUD operations in TodoController.

---
+++ ToDoApi/Controllers/TodoController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.FirstOrDefault(x => x.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return new ObjectResult(item);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                _context.TodoItems.Add(item);
+                _context.SaveChanges();
+            }
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Id != id)
+            {
+                BadRequest();
+            }
+            var temp = _context.TodoItems.FirstOrDefault(x => x.Id == id);
+            if (temp == null)
+            {
+                return NotFound();
+            }
+
+            temp.IsComplete = item.IsComplete;
+            temp.Name = item.Name;
+
+            _context.TodoItems.Update(temp);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var temp = _context.TodoItems.FirstOrDefault(x => x.Id == id);
+            if(temp == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(temp);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+    }
+}
